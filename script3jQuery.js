9747aa421c06f33e93e6c3f8c7691efcde5ffe2c
Сергій Хомин <43818326+Serega346@users.noreply.github.com>
2018-11-12 21:43:24 +0200

    Update script3jQuery.js

--- script3jQuery.js
+++ script3jQuery.js
@@ -1,16 +1,12 @@
 alert($("#elem").html());
 
  $( document ).ready(function() {
+	 
       $('#list-header').css({ 'font-weight': 'bolder' });
-	   $('#color').css({ 'color': '#555' });
-	  var zamina = $("#name").html().replace('Peter','SEREGA');
-	  $("#name").html(zamina);
+      $('#color').css({ 'color': '#555' });
+var zamina = $("#name").html().replace('Peter','SEREGA');
+      $("#name").html(zamina);
+	 
     });
 	
- /*$( document ).ready(function() {
-      $('#color').css({ 'color': '#555' });
-	  var zamina = $("#name").html().replace('Peter','SEREGA');
-	  $("#name").html(zamina);
-    });
-	*/
 	
