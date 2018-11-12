48df64b51c07f17a6ca566b50335bb3a2927adbc
Сергій Хомин <43818326+Serega346@users.noreply.github.com>
2018-11-12 21:42:12 +0200

    Add files via upload

---
+++ script3jQuery.js
@@ -0,0 +1,16 @@
+alert($("#elem").html());
+
+ $( document ).ready(function() {
+      $('#list-header').css({ 'font-weight': 'bolder' });
+	   $('#color').css({ 'color': '#555' });
+	  var zamina = $("#name").html().replace('Peter','SEREGA');
+	  $("#name").html(zamina);
+    });
+	
+ /*$( document ).ready(function() {
+      $('#color').css({ 'color': '#555' });
+	  var zamina = $("#name").html().replace('Peter','SEREGA');
+	  $("#name").html(zamina);
+    });
+	*/
+	
